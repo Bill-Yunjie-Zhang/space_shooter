8825caa0b716d1daf6d62bfb1b36167fb55683dd
Bill <207559339@qq.com>
2020-04-09 16:01:04 +0800

    almost there

--- main.py
+++ main.py
@@ -13,9 +13,11 @@ BLUE = (0, 0, 255)
 #screen
 SCREEN_WIDTH = 300
 SCREEN_HEIGHT = 600
+SCREEN_COLOR = WHITE
 SCREEN_SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
 #player
 PLAYER_RADIUS = 10
+PLAYER_SPEED = 2
 PLAYER_INITIAL_X_LOCATION = int(SCREEN_WIDTH / 2)
 PLAYER_INITIAL_Y_LOCATION = SCREEN_HEIGHT - PLAYER_RADIUS
 #bullets
@@ -23,8 +25,9 @@ BULLETS_WIDTH = 5
 BULLETS_HEIGHT = 10
 #enemies
 ENEMY_RANDIUS = 10
-NUMBER_OF_ENEMIES = 100
+NUMBER_OF_ENEMIES = 50
 ENEMY_SPEED = 1
+ENEMY_COLOR = BLACK
 X_RANGE_MIN = ENEMY_RANDIUS
 X_RANGE_MAX = SCREEN_WIDTH - ENEMY_RANDIUS
 Y_RANGE_MIN = -1000
@@ -34,7 +37,7 @@ BULLET_SPEED = 3
 
 def draw_bullets(bullets, bullet_motion, screen):
     for bullet in bullets:
-        pygame.draw.rect(screen, BLACK, (bullet.x - (BULLETS_WIDTH / 2), bullet.y, BULLETS_WIDTH, BULLETS_HEIGHT))
+        pygame.draw.rect(screen, ENEMY_COLOR, (bullet.x - (BULLETS_WIDTH / 2), bullet.y, BULLETS_WIDTH, BULLETS_HEIGHT))
         bullet.move(bullet_motion)
 
 def create_enemies(num):
@@ -45,7 +48,7 @@ def create_enemies(num):
 
 def draw_enemies(enemies, enemy_motion, screen):
     for enemy in enemies:
-        pygame.draw.circle(screen, BLACK, enemy, ENEMY_RANDIUS)
+        pygame.draw.circle(screen, ENEMY_COLOR, enemy, ENEMY_RANDIUS)
         enemy.move(enemy_motion)
 
 def main():
@@ -62,30 +65,42 @@ def main():
     enemy_motion = freegames.vector(0, ENEMY_SPEED)
 
     done = False
+    end = False
     while not done:
+        PLAYER_COLOR = BLUE
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
-            elif event.type == pygame.KEYDOWN:
-                print(player_loc.x)
-                print(PLAYER_RADIUS)
-                if player_loc.x > PLAYER_RADIUS and player_loc.x < SCREEN_WIDTH - PLAYER_RADIUS:
-                    if event.key == pygame.K_LEFT:
-                        player_motion.x = -1
-                    elif event.key == pygame.K_RIGHT:
-                        player_motion.x = 1
-                elif event.key == pygame.K_SPACE:
-                    bullets.append(player_loc.copy())
-                
-            elif event.type == pygame.KEYUP:
-                if (event.key == pygame.K_LEFT or
-                    event.key == pygame.K_RIGHT):
-                    player_motion.x = 0
-        screen.fill(RED)  # same as clearing the screen
+            if not end:
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT and player_loc.x > PLAYER_RADIUS:
+                        player_motion.x = -PLAYER_SPEED
+                    elif event.key == pygame.K_RIGHT and player_loc.x < SCREEN_WIDTH - PLAYER_RADIUS:
+                        player_motion.x = PLAYER_SPEED
+                    elif event.key == pygame.K_SPACE:
+                        bullets.append(player_loc.copy())
+                elif event.type == pygame.KEYUP:
+                    if (event.key == pygame.K_LEFT or
+                        event.key == pygame.K_RIGHT):
+                        player_motion.x = 0
+        screen.fill(SCREEN_COLOR)  # same as clearing the screen
         draw_enemies(enemies, enemy_motion, screen)
-
         draw_bullets(bullets, bullet_motion, screen)
-        pygame.draw.circle(screen, BLUE, player_loc, PLAYER_RADIUS)
+        for enemy in enemies:
+            for bullet in bullets:
+                if (enemy.x - ENEMY_RANDIUS) < (bullet.x - BULLETS_WIDTH) and (enemy.x + ENEMY_RANDIUS) > bullet.x and enemy.y >= bullet.y:
+                    enemies.remove(enemy)
+                    bullets.remove(bullet)
+            if enemy.y >= SCREEN_HEIGHT:
+                end = True
+        
+        if end:
+            enemy_motion.y = 0
+            bullet_motion.y = 0
+            player_motion.x = 0
+            PLAYER_COLOR = RED
+
+        pygame.draw.circle(screen, PLAYER_COLOR, player_loc, PLAYER_RADIUS)
         player_loc.move(player_motion)
         pygame.display.update()
         clock.tick(60)
